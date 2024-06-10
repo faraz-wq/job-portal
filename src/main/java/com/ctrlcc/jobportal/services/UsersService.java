d48265d938cf3268f79e9ae06aa36ac2896075f0
Faraz <faraz40684@gmail.com>
2024-06-10 13:11:24 +0530

    Dashboard Integration for currently logged in user

--- src/main/java/com/ctrlcc/jobportal/services/UsersService.java
+++ src/main/java/com/ctrlcc/jobportal/services/UsersService.java
@@ -2,6 +2,11 @@ package com.ctrlcc.jobportal.services;
 
 import java.util.Date;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -45,8 +50,24 @@ public class UsersService {
     }
 
     public Object getCurrentUserProfile() {
-        // TODO Auto-generated method stub
+        Authentication authentication = SecurityContextHolder.getContext()
+        .getAuthentication();
         
-        throw new UnsupportedOperationException("Unimplemented method 'getCurrentUserProfile'");
+        if(!(authentication instanceof AnonymousAuthenticationToken)) {
+            String username = authentication.getName();
+            Users users = usersRepository.findByEmail(username).orElseThrow(()->new
+                        UsernameNotFoundException("Could not find user."));
+            int userId = users.getUserId();
+            if(authentication.getAuthorities().contains(new SimpleGrantedAuthority("recruiter")))   {
+                RecruiterProfile recruiterProfile = recruiterProfileRepository.findById(userId)
+                                                    .orElse(new RecruiterProfile());
+                return recruiterProfile;
+            }   else    {
+                JobSeekerProfile jobSeekerProfile = jobSeekerProfileRepository.findById(userId)
+                                                    .orElse(new JobSeekerProfile());
+                return jobSeekerProfile;
+            }
+        }
+        return null;
     }
 }
